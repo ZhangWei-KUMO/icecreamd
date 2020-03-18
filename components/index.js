c9e4165736e6ec659043812811eeab6d5e9df727
ZhangWei-KUMO <kanseefoil@gmail.com>
2020-03-18 18:46:50 +0800

    release 0.0.6

--- components/index.js
+++ components/index.js
@@ -1,4 +1,4 @@
 export { default as Card } from './Card';
 export { default as Alert } from './Alert';
-export { default as Gradient } from './Gradient';
+export { default as Gradient } from './GradientBar';
 export { default as Icon } from './Icon';
