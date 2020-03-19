3cfca94f236aa1aae0ca1ed6e46b7fba2442ab68
ZhangWei-KUMO <kanseefoil@gmail.com>
2020-03-20 00:01:35 +0800

    add

--- es/index.js
+++ es/index.js
@@ -1,5 +1,5 @@
 export { default as Alert } from './Alert';
-export { default as Gradient } from './GradientBar';
+export { default as GradientBar } from './GradientBar';
 export { default as Icon } from './Icon';
 export { default as Header } from './Header';
 export { default as Button } from './Button';
