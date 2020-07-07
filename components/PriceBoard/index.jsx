62147eaf0c98edb489fc539aa623aacd62c4eff1
ZhangWei-KUMO <kanseefoil@gmail.com>
2020-07-07 15:25:07 +0800

    add auto save format

--- components/PriceBoard/index.jsx
+++ components/PriceBoard/index.jsx
@@ -1,69 +1,23 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 
-import './index.less';
-const style = {
-  height: "800px"
-}
-class Iconcard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: props.items,
-      mouseEnter: false
-    }
-  }
-
-  static propTypes = {
-    prefixCls: PropTypes.string,
-    title: PropTypes.string,
-    link: PropTypes.string,
-    background: PropTypes.string,
-    icon: PropTypes.string,
-    iconColor: PropTypes.string,
-    color: PropTypes.string
-  };
-
-  static defaultProps = {
-    prefixCls: "ice",
-    title: "",
-    link: "#",
-    background: "https://marketplace-images.githubusercontent.com/703/15031fc4-e40e-11e7-9e6c-42cd55ee6584?auto=webp&format=jpeg&width=600",
-    icon: "https://avatars1.githubusercontent.com/ml/283?s=106&v=4",
-    iconColor: "#45D298",
-    color: "#000"
-  }
-
-  handleMouseEnter = () => {
-    this.setState({
-      mouseEnter: true
-    })
-  }
-
-  handleMouseEnter = () => {
-    this.setState({
-      mouseEnter: true
-    })
-  }
-
-  handleMouseLeave = () => {
-    this.setState({
-      mouseEnter: false
-    })
-  }
+class Iconcard extends PureComponent {
   render() {
-    let { prefix, title, link, background, icon, iconColor, color } = this.props;
-    let { mouseEnter } = this.state;
+    const {
+      prefixCls, title, link, background, icon, iconColor, color
+    } = this.props;
     return (
-      <Fragment>
-        <a href={link}
+      <>
+        <a
+          href={link}
           target="__blank"
-          onMouseEnter={this.handleMouseEnter}
-          onMouseLeave={this.handleMouseLeave}
         >
-          <div className={`${prefix}-iconcard`}
-            style={{ backgroundImage: `url(${background})` }}>
-            <div className={`${prefix}-ciclebadget ${prefix}-ciclebadget-float`}
+          <div
+            className={`${prefixCls}-iconcard`}
+            style={{ backgroundImage: `url(${background})` }}
+          >
+            <div
+              className={`${prefixCls}-ciclebadget ${prefixCls}-ciclebadget-float`}
               style={{ backgroundColor: `${iconColor}` }}
             >
               <img src={icon} alt="浪视频" />
@@ -71,9 +25,29 @@ class Iconcard extends Component {
             <h2 style={{ color }}>{title}</h2>
           </div>
         </a>
-      </Fragment >
-    )
+      </>
+    );
   }
 }
 
-export default Iconcard
+Iconcard.propTypes = {
+  prefixCls: PropTypes.string,
+  title: PropTypes.string,
+  link: PropTypes.string,
+  background: PropTypes.string,
+  icon: PropTypes.string,
+  iconColor: PropTypes.string,
+  color: PropTypes.string
+};
+
+Iconcard.defaultProps = {
+  prefixCls: "ice",
+  title: "",
+  link: "#",
+  background: "https://marketplace-images.githubusercontent.com/703/15031fc4-e40e-11e7-9e6c-42cd55ee6584?auto=webp&format=jpeg&width=600",
+  icon: "https://avatars1.githubusercontent.com/ml/283?s=106&v=4",
+  iconColor: "#45D298",
+  color: "#000"
+};
+
+export default Iconcard;
--- components/index.js
+++ components/index.js
@@ -1,8 +1,8 @@
-export { default as Alert } from './Alert';
-export { default as GradientBar } from './GradientBar';
-export { default as Header } from './Header';
-export { default as Button } from './Button';
-export { default as Card } from './Card';
-export { default as Footer } from './Footer';
-export { default as Modal } from './Modal';
-export { default as PriceBoard } from './PriceBoard';
+export { default as Alert } from "./Alert";
+export { default as GradientBar } from "./GradientBar";
+export { default as Header } from "./Header";
+export { default as Button } from "./Button";
+export { default as Card } from "./Card";
+export { default as Footer } from "./Footer";
+export { default as Modal } from "./Modal";
+export { default as PriceBoard } from "./PriceBoard";
