2156ed91f186b250e9be628011c544f6ea6173a5
ZhangWei-KUMO <kanseefoil@gmail.com>
2020-07-07 13:43:54 +0800

    strength styles

--- components/Alert/index.jsx
+++ components/Alert/index.jsx
@@ -4,11 +4,11 @@ class Alert extends Component {
     return (
       <div className="alert">
         <div>
-          <img src={this.props.image} />
+          <img src={this.props.image} className="alert-img"/>
         </div>
         <div className="alert-container">
-          <h3>{this.props.title}</h3>
-          <p>{this.props.content}</p>
+          <h3 className="alert-title">{this.props.title}</h3>
+          <p className="alert-content">{this.props.content}</p>
         </div>
       </div>
     )
--- components/GradientBar/index.jsx
+++ components/GradientBar/index.jsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-class Card extends Component {
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+class GradientBar extends Component {
+  static propTypes = {
+    prefixCls: PropTypes.string,
+    title: PropTypes.string,
+    content: PropTypes.string,
+    moment: PropTypes.string,
+    image: PropTypes.string,
+    link: PropTypes.string,
+  };
+
+  static defaultProps = {
+    prefixCls: 'ice',
+    title: '请填写标题',
+    content: '请填写文章内容',
+    image: 'https://gw.alipayobjects.com/mdn/rms_08e378/afts/img/A*yQqmQ514NNIAAAAAAAAAAABkARQnAQ',
+    moment: '2020-3-20',
+    link: "/#"
+  };
+
   render() {
+    let { prefixCls, title, content, image, moment, link } = this.props;
+    let barName = classNames({ [`${prefixCls}-bar`]: true });
+    let containerName = classNames({ [`${prefixCls}-card-container`]: true });
     return (
-      <div className="gradient-bar">
+      <div className={barName}>
         <div>
           <img src={this.props.image} />
         </div>
-        <div className="gradien-bar-container">
+        <div className={containerName}>
           <h3>{this.props.title}</h3>
           <p>{this.props.content}</p>
         </div>
@@ -22,4 +46,4 @@ class Card extends Component {
   }
 }
 
-export default Card;
+export default GradientBar;
